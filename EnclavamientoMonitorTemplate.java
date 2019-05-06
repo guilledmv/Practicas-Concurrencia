039e21a43710b2830a20764e8ef1adbaf4fd4f31
guilledmv <45519499+guilledmv@users.noreply.github.com>
2019-05-06 13:26:04 +0200

    Create EnclavamientoMonitorTemplate.java

---
+++ EnclavamientoMonitorTemplate.java
@@ -0,0 +1,67 @@
+package Practica1;
+
+import es.upm.babel.cclib.Monitor;
+
+//Grupo: Guillermo De Miguel Villanueva (160262) , Marcos Arevalo
+
+public class EnclavamientoMonitorTemplate implements Enclavamiento {
+
+  Monitor mutex = new Monitor();
+  ControlSimulado c = new ControlSimulado ();
+  
+  @Override
+  public void avisarPresencia(boolean presencia) {
+    mutex.enter();
+    // chequeo de la PRE
+    // chequeo de la CPRE y posible bloqueo
+    // implementacion de la POST
+    c.detectarPresencia(false); //Bloque hasta detectar que no hay ningun coche en el cruce
+    
+    // codigo de desbloqueo
+    mutex.leave();
+  }
+
+  @Override
+  public boolean leerCambioBarrera(boolean actual) {
+    mutex.enter();
+    // chequeo de la PRE
+    // chequeo de la CPRE y posible bloqueo
+    // implementacion de la POST
+    // codigo de desbloqueo
+    mutex.leave();
+    return false;
+  }
+
+  @Override
+  public boolean leerCambioFreno(boolean actual) {
+    mutex.enter();
+    // chequeo de la PRE
+    // chequeo de la CPRE y posible bloqueo
+    // implementacion de la POST
+    // codigo de desbloqueo
+    mutex.leave();
+    return false;
+  }
+
+  @Override
+  public Control.Color leerCambioSemaforo(int i, Control.Color actual) {
+    mutex.enter();
+    // chequeo de la PRE
+    // chequeo de la CPRE y posible bloqueo
+    // implementacion de la POST
+    // codigo de desbloqueo
+    mutex.leave();
+    return null;
+  }
+
+  @Override
+  public void avisarPasoPorBaliza(int i) {
+    mutex.enter();
+    // chequeo de la PRE
+    // chequeo de la CPRE y posible bloqueo
+    // implementacion de la POST
+    // codigo de desbloqueo
+    mutex.leave();
+  }
+ 
+}
