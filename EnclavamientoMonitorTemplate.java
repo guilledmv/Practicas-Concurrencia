f63f5d652e80ffeb01e35760e4728dd2aea6a750
guilledmv <45519499+guilledmv@users.noreply.github.com>
2019-05-11 19:00:45 +0200

    Variables declaradas e inicializadas

--- EnclavamientoMonitorTemplate.java
+++ EnclavamientoMonitorTemplate.java
@@ -6,8 +6,27 @@ import es.upm.babel.cclib.Monitor;
 
 public class EnclavamientoMonitorTemplate implements Enclavamiento {
 
+//Declaro variables necesarias para el programa
+
   Monitor mutex = new Monitor();
-  ControlSimulado c = new ControlSimulado ();
+  
+  private boolean presencia; // Declaramos booleano para la presencia
+  
+  private int [] tren = {0,1,2,3}; // Declaramos un array de tren, para saber a que tren se refiere
+  
+  private enum color {Rojo,Amarillo,Verde}; // Declaramos un tipo enumerado para los colores del semaforo
+  
+  
+  public EnclavamientoMonitorTemplate(int[] tren) {
+	  
+	  this.presencia = false; // Inicializamos la presencia--> false
+	  
+	  this.tren = new int[0]; // Inicializamos al tren 0                // INICIAL
+	  
+	  color c = color.Verde; // Inicializamos al color verde
+	    
+  }
+  
   
   @Override
   public void avisarPresencia(boolean presencia) {
@@ -15,7 +34,7 @@ public class EnclavamientoMonitorTemplate implements Enclavamiento {
     // chequeo de la PRE
     // chequeo de la CPRE y posible bloqueo
     // implementacion de la POST
-    c.detectarPresencia(false); //Bloque hasta detectar que no hay ningun coche en el cruce
+   
     
     // codigo de desbloqueo
     mutex.leave();
