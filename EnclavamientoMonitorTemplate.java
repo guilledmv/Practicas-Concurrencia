30af4ffa74eb82c5c826a29cd4940c454b3ae9ec
Marcos Arévalo Salas <50323363+MarcosM291@users.noreply.github.com>
2019-05-13 13:29:14 +0200

    PRES FINALIZADOS

--- EnclavamientoMonitorTemplate.java
+++ EnclavamientoMonitorTemplate.java
@@ -1,9 +1,7 @@
 import java.util.ArrayList;
 
 import java.util.HashMap;
-
 import java.util.Map;
-
 import es.upm.babel.cclib.Monitor;
 
 //Grupo: Guillermo De Miguel Villanueva (160262) , Marcos Arevalo Salas (160266)
@@ -29,30 +27,27 @@ public class EnclavamientoMonitor implements Enclavamiento {
   public EnclavamientoMonitor() {
 	  
 	  this.presencia = false; // Inicializamos la presencia--> false  
-	  this.coloresBaliza = new String [] {"Verde","Verde","Verde","Verde"};
+	  this.coloresBaliza = new String [] {"VERDE","VERDE","VERDE","VERDE"};
 	  for ( Integer i = 1; i<4;i++) {
 		  trenes.put(i,0); // Inicializamos a 0, el numero de trenes detras de la baliza
-	  }
-	  
-	
-	    
+	  }    
   }
   
   // Metodo auxiliar con los colores correctos
   private void coloresCorrectos () {
 	// Implementacion colores correctos
 	   if( trenes.get(1)>0 ) {
-		   coloresBaliza[1]= "Rojo";
+		   coloresBaliza[1]= "ROJO";
 	   } else if( trenes.get(1) == 0 && ( trenes.get(2)>0 || presencia == true)) {
-		   coloresBaliza[1] = "Amarillo";
+		   coloresBaliza[1] = "AMARILLO";
 	   } else if( trenes.get(1) == 0 && trenes.get(2) == 0 && presencia == false) {
-		   coloresBaliza[1]= "Verde";
+		   coloresBaliza[1]= "VERDE";
 	   } else if( trenes.get(2)>0 || presencia == true) {
-		   coloresBaliza[2] = "Rojo";
+		   coloresBaliza[2] = "ROJO";
 	   } else if( trenes.get(2) == 0 && presencia == false) {
-		   coloresBaliza[2] = "Verde";
+		   coloresBaliza[2] = "VERDE";
 	   } else {
-		   coloresBaliza[3] = "Verde";
+		   coloresBaliza[3] = "VERDE";
 	   }
   }
   
@@ -62,12 +57,14 @@ public class EnclavamientoMonitor implements Enclavamiento {
     // chequeo de la PRE
     // chequeo de la CPRE y posible bloqueo
     // implementacion de la POST
+    
+   //------- PRE == CIERTO -------
+    
+  //------- POST -------
    this.presencia = presencia;
-   // Falta por programar la condicion--> self.tren = self.PRE.tren 
    coloresCorrectos();
-    // codigo de desbloqueo
     mutex.leave();
-  }
+     }
 
   @Override
   public boolean leerCambioBarrera(boolean actual) {
@@ -76,6 +73,24 @@ public class EnclavamientoMonitor implements Enclavamiento {
     // chequeo de la CPRE y posible bloqueo
     // implementacion de la POST
     // codigo de desbloqueo
+    
+    //------- SI NO SE CUMPLE LA CPRE -> EXCEPCION -------
+    
+    boolean resul=false;
+    if(this.trenes.get(1)+this.trenes.get(2)==0) {
+    	resul=true;
+    }
+    if (actual==resul) {
+		mutex.leave();
+		throw new PreconditionFailedException();
+	}
+    
+  //------- POST -------
+  
+    
+    
+    
+    
     mutex.leave();
     return false;
   }
@@ -87,29 +102,68 @@ public class EnclavamientoMonitor implements Enclavamiento {
     // chequeo de la CPRE y posible bloqueo
     // implementacion de la POST
     // codigo de desbloqueo
+    
+    //------- SI NO SE CUMPLE LA CPRE -> EXCEPCION -------
+    
+    boolean resul=false;
+    if(this.trenes.get(1)>1 || this.trenes.get(2)>1 || this.trenes.get(2)==1 && this.presencia==true) {
+    	resul=true;
+    }
+
+    if (actual==resul) {
+		mutex.leave();
+		throw new PreconditionFailedException();
+	}
+    
+  //------- POST -------
+    
+    
     mutex.leave();
     return false;
   }
 
   @Override
   public Control.Color leerCambioSemaforo(int i, Control.Color actual) {
-    mutex.enter();
-    // chequeo de la PRE
-    // chequeo de la CPRE y posible bloqueo
-    // implementacion de la POST
-    // codigo de desbloqueo
-    mutex.leave();
-    return null;
-  }
+	    mutex.enter();
+	    // chequeo de la PRE
+	    // chequeo de la CPRE y posible bloqueo
+	    // implementacion de la POST
+	    // codigo de desbloqueo
+	    
+	    //------- SI NO SE CUMPLE LA PRE Y CPRE -> EXCEPCION -------
+	    
+	    if (actual.equals(this.coloresBaliza.equals(i))&& i==0) {
+			mutex.leave();
+			throw new PreconditionFailedException();
+		}
+	    
+	  //------- POST -------
+	    
+	    
+	    mutex.leave();
+	    return null;
+	  }
 
   @Override
   public void avisarPasoPorBaliza(int i) {
-    mutex.enter();
-    // chequeo de la PRE
-    // chequeo de la CPRE y posible bloqueo
-    // implementacion de la POST
-    // codigo de desbloqueo
-    mutex.leave();
-  }
+	    mutex.enter();
+	    // chequeo de la PRE
+	    // chequeo de la CPRE y posible bloqueo
+	    // implementacion de la POST
+	    // codigo de desbloqueo
+	    
+	    //------- SI NO SE CUMPLE LA PRE Y CPRE -> EXCEPCION -------
+	    
+	    if (i==0) {
+			mutex.leave();
+			throw new PreconditionFailedException();
+		}
+	    
+	  //------- POST -------
+	    
+	    
+	    mutex.leave();
+	   
+	  }
  
 }
